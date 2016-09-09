ee38da68a02b0b8015438d951fa86b228d58a6a9
付泉 <3040408640@qq.com>
2016-09-09 09:41:41 +0800

    init

---
+++ book-example/src/main/java/ex02/pyrmont/Constants.java
@@ -0,0 +1,21 @@
+package ex02.pyrmont;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class Constants {
+  public static final String WEB_ROOT =
+          System.getProperty("user.dir") + File.separator + "book-example" + File.separator + "webroot";
+
+  public static void main(String[] args) throws IOException {
+    File classPath = new File(Constants.WEB_ROOT);
+
+    // the forming of repository is taken from the createClassLoader method in
+    // org.apache.catalina.startup.ClassLoaderFactory
+    String repository = (new URL("file", null, classPath.getCanonicalPath() + File.separator)).toString();
+
+    System.out.println(repository);
+
+  }
+}
---
+++ book-example/src/main/java/test/CommonTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.io.File;
+
+/**
+ * fuquanemail@gmail.com
+ */
+public class CommonTest {
+    public static void main(String[] args) {
+
+        String WEB_ROOT =
+                System.getProperty("user.dir") + File.separator + "webroot";
+        System.out.println(WEB_ROOT);
+    }
+}
